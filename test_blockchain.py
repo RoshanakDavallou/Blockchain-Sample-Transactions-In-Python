4e516d600be89e7de3bae7d2544de8f7a2d5b6e3
Roshanak Davallou <118665343+RoshanakDavallou@users.noreply.github.com>
2025-04-20 11:44:04 +0200

    Updated test_blockchain.py

--- test_blockchain.py
+++ test_blockchain.py
@@ -18,7 +18,6 @@ class TestBlockchain(unittest.TestCase):
         self.token_contract.distribute_tokens(self.retailer_wallet.get_address(), 200)
 
     def test_block_creation(self):
-        # Test the addition and validation of a new block
         self.blockchain.add_new_transaction("<sample>XML transaction</sample>")
         initial_length = len(self.blockchain.chain)
         self.blockchain.mine()
@@ -28,37 +27,34 @@ class TestBlockchain(unittest.TestCase):
         self.assertEqual(new_block.index, initial_length)
 
     def test_genesis_block(self):
-        # Test the creation of the genesis block
         genesis_block = self.blockchain.chain[0]
         self.assertEqual(genesis_block.index, 0)
         self.assertEqual(genesis_block.previous_hash, '0')
 
     def test_new_transaction(self):
-        # Test adding a new transaction
         initial_length = len(self.blockchain.unconfirmed_transactions)
         self.blockchain.add_new_transaction("<sample>XML transaction</sample>")
         self.assertEqual(len(self.blockchain.unconfirmed_transactions), initial_length + 1)
 
     def test_mining(self):
-        # Test mining a new block with transactions
         self.blockchain.add_new_transaction("<sample>XML transaction</sample>")
         last_block_index = self.blockchain.last_block.index
         self.blockchain.mine()
         self.assertEqual(self.blockchain.last_block.index, last_block_index + 1)
 
     def test_proof_of_work(self):
-        # Test the proof of work algorithm
         last_block = self.blockchain.last_block
-        new_block = Block(index=last_block.index + 1,
-                          transactions=["<sample>XML transaction</sample>"],
-                          timestamp=time.time(),
-                          previous_hash=last_block.hash)
+        new_block = Block(
+            index=last_block.index + 1,
+            transactions=["<sample>XML transaction</sample>"],
+            timestamp=time.time(),
+            previous_hash=last_block.hash
+        )
         proof = self.blockchain.proof_of_work(new_block)
         self.assertTrue(self.blockchain.is_valid_proof(new_block, proof))
         self.assertTrue(proof.startswith('0000'))
 
     def test_token_transfer(self):
-        # Test token transfer between wallets
         initial_balance_supplier = self.token_contract.get_balance(self.supplier_wallet.get_address())
         initial_balance_carrier = self.token_contract.get_balance(self.carrier_wallet.get_address())
         self.token_contract.transfer_tokens(
@@ -69,8 +65,34 @@ class TestBlockchain(unittest.TestCase):
         self.assertEqual(self.token_contract.get_balance(self.supplier_wallet.get_address()), initial_balance_supplier - 50)
         self.assertEqual(self.token_contract.get_balance(self.carrier_wallet.get_address()), initial_balance_carrier + 50)
 
+    def test_overspending_tokens(self):
+        with self.assertRaises(ValueError):
+            self.token_contract.transfer_tokens(
+                from_addr=self.supplier_wallet.get_address(),
+                to=self.carrier_wallet.get_address(),
+                amount=10000  # exceeds balance
+            )
+
+    def test_invalid_block_hash(self):
+        self.blockchain.add_new_transaction("<data>Test</data>")
+        tampered_block = Block(
+            index=self.blockchain.last_block.index + 1,
+            transactions=["<data>Fake</data>"],
+            timestamp=time.time(),
+            previous_hash="wrong_hash"
+        )
+        fake_proof = tampered_block.compute_hash()
+        result = self.blockchain.add_block(tampered_block, fake_proof)
+        self.assertFalse(result)
+
+    def test_invalid_xml_transaction(self):
+        corrupt_xml = "<order><brokenTag>"
+        self.blockchain.add_new_transaction(corrupt_xml)
+        self.blockchain.mine()
+        last_block = self.blockchain.last_block
+        self.assertIn(corrupt_xml, last_block.transactions)
+
     def test_wallet_creation(self):
-        # Test wallet creation and address uniqueness
         wallet_1 = DigitalWallet("TestUser1")
         wallet_2 = DigitalWallet("TestUser2")
         self.assertNotEqual(wallet_1.get_address(), wallet_2.get_address())
